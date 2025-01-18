df019e961ce66f4324b49d1deb6aaa5394a77c53
vanns <vannsanity@gmail.com>
2025-01-18 13:44:19 -0500

    Changes made. Loot tables successful!

--- src/main/java/net/vannsanity/jurassicoverhaul/item/ModCreativeModTabs.java
+++ src/main/java/net/vannsanity/jurassicoverhaul/item/ModCreativeModTabs.java
@@ -14,21 +14,22 @@ public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, JurassicOverhaul.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> JURASSIC_OVERHAUL_ITEMS = CREATIVE_MODE_TABS.register("jurassic_overhaul_items",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.AMBER.get()))
-                    .title(Component.translatable("crativetab.jurassic_overhaul_items"))
-                    .displayItems((itemDisplayParameters, output) -> {
-                        output.accept(ModItems.AMBER.get());
-
-
-                    })
-                    .build());
 
     public static final RegistryObject<CreativeModeTab> JURASSIC_OVERHAUL_BLOCKS = CREATIVE_MODE_TABS.register("jurassic_overhaul_blocks",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.AMBER_BLOCK.get()))
                     .title(Component.translatable("crativetab.jurassic_overhaul_blocks"))
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModBlocks.AMBER_BLOCK.get());
+
+
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> JURASSIC_OVERHAUL_ITEMS = CREATIVE_MODE_TABS.register("jurassic_overhaul_items",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.AMBER.get()))
+                    .title(Component.translatable("crativetab.jurassic_overhaul_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.AMBER.get());
 
 
                     })
